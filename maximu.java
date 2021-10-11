9d8dcf1e792138ba2602c27d33f8f49a6d91160c
Abhi8358 <83907876+Abhi8358@users.noreply.github.com>
2021-10-11 23:46:45 +0530

    Add files via upload

---
+++ maximu.java
@@ -0,0 +1,28 @@
+
+public class maximu {
+	public static void main(String argu[]) {
+		int a=23;
+		int b=43;
+		int c=8;
+		if (a>b){
+			if (a>c) {
+				System.out.print(a);
+			
+			}
+		}
+		if (b>a){
+			if (b>c) {
+				System.out.print(b);
+			
+			}
+		}
+		if (c>b){
+			if (c>a) {
+				System.out.print(c);
+			
+			}
+		}
+		
+	}
+
+}
